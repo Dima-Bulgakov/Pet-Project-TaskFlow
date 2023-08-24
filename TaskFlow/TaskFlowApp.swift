e2ddcc2ce2d185dd745a323a5011f1804595a500
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-08-24 12:11:45 +0300

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Dima on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
