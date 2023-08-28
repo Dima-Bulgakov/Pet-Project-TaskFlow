bd6dc9a5e19e60be47280af424d094f6cf0159ad
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-08-28 12:19:20 +0300

    commit 3

--- TaskFlow/View/Tasks/TaskListView.swift
+++ TaskFlow/View/Tasks/TaskListView.swift
@@ -46,7 +46,6 @@ struct TaskListView: View {
                                                  Image(systemName: "plus")
                                              }))
             }
-                
         }
         .navigationTitle("mainTitle")
     }
